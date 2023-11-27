ec140d0ea7e1110f3491daad1ce152518bc1c81b
Quazi Samiha <quazisamiha@gmail.com>
2023-11-27 09:18:13 -0800

    react parallax added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
-import menuImg from "../../../assets/menu/menu-bg.png";
+// import menuImg from "../../../assets/menu/menu-bg.png";
+import menuImg from "../../../assets/menu/banner3.jpg";
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 function Menu() {
   return (
@@ -9,6 +10,7 @@ function Menu() {
         <Helmet>
           <title>Bistro Boss | Menu</title>
         </Helmet>
+        <PopularMenu />
         <Cover img={menuImg} title="Our Menu" />
         <PopularMenu />
       </section>
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,19 +1,32 @@
+import { Parallax } from "react-parallax";
+
 function Cover({ img, title }) {
   return (
     <>
-      <div className="h-[700px]" style={{ backgroundImage: `url("${img}")` }}>
-        <div className="bg-opacity-60"></div>
-        <div className="text-center text-neutral-200">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-            <p className="mb-5">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae
-              pariatur dolores ipsam repellat odio corrupti quo impedit eius!
-              Magni, modi.
-            </p>
+      <Parallax
+        blur={{ min: -50, max: 50 }}
+        // bgImage={require("path/to/another/image.jpg")}
+        bgImage={img}
+        bgImageAlt="the menu"
+        strength={-200}
+      >
+        {/* <div className="h-[700px]" style={{ backgroundImage: `url("${img}")` }}> */}
+        <div className="h-[700px] mt-16 flex items-center justify-center">
+          <div className="bg-opacity-40"></div>
+          <div className="text-center text-neutral-100">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+              <p className="mb-5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae
+                pariatur dolores ipsam repellat odio corrupti quo impedit eius!
+                Magni, modi.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+        {/* Blur transition from min to max
+        <div style={{ height: "200px" }} /> */}
+      </Parallax>
     </>
   );
 }
